1c14ecb544cd18646364e38072e5114c51a0969e
Lucas Messias <luquinhasmessias7@gmail.com>
2024-12-05 14:23:50 -0300

    edicao de menu e forma de iniciar

--- code/game.py
+++ code/game.py
@@ -1,33 +1,33 @@
 import pygame
+from code.menu import Menu
+
 
 class Game:
 
     def __init__(self):
-        self.window = None
         pygame.init()
-        screen = pygame.display.set_mode((800,600))
+        self.screen = pygame.display.set_mode((800,600))
 
     
 
     def run(self):
 
-        clock = pygame.time.Clock()
-        running = True
 
-        while running:
+        while True: 
+            menu = Menu(self.screen)
+            menu.run()
             # poll for events
             # pygame.QUIT event means the user clicked X to close your window
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    running = False
+                    pygame.quit()
+                    quit()
 
             # fill the screen with a color to wipe away anything from last frame
 
             # RENDER YOUR GAME HERE
 
             # flip() the display to put your work on screen
-            pygame.display.flip()
-
-            clock.tick(60)  # limits FPS to 60
+            pygame.display.flip() # limits FPS to 60
 
         pygame.quit()
---
+++ code/menu.py
@@ -0,0 +1,8 @@
+class Menu():
+
+    def __init__(self,screen):
+        self.screen = screen
+
+
+    def run(self):
+        pass
