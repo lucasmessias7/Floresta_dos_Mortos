c9841499b9fc7c7dc02caa34751a375e5b037db4
LUCAS MESSIAS ALVES <539555318@educacao.intranet>
2024-12-05 18:59:17 -0300

    adição do primeiro nome

--- code/Const.py
+++ code/Const.py
@@ -1,2 +1,12 @@
+import pygame
+pygame.init()
+pygame.font.init()
+
+
 JAN_LARGURA = 600
 JAN_ALTURA = 600
+TEXT_FONT = pygame.font.Font('assets/fonts/Pixelmax-Regular.otf',80)
+TEXT_1 = 'Floresta'
+TEXT_2 = 'Dos Mortos'
+TEXT_COLOR = (255,255,255)
+TEXT_CENTER_POSITION = ((JAN_LARGURA / 2), 200 )
--- code/menu.py
+++ code/menu.py
@@ -1,4 +1,12 @@
 import pygame
+from pygame import Surface, Rect
+from pygame.font import Font
+from code.Const import JAN_ALTURA, JAN_LARGURA, TEXT_COLOR, TEXT_1, TEXT_CENTER_POSITION, TEXT_FONT, TEXT_2
+
+
+# pygame.init()
+# pygame.font.init()
+
 class Menu():
 
     def __init__(self,screen):
@@ -12,7 +20,16 @@ class Menu():
         pygame.mixer_music.play(-1)
 
         while True:
-            self.screen.blit(source=self.background, dest=self.rect)
+            self.screen.blit(source=self.background, dest=self.rect) 
+            
+            
+            # self.menu_text(text_size=70,text='Floresta',text_color=(255,255,255), text_center_pos=((JAN_LARGURA / 2), 90))
+            text_one = TEXT_FONT.render(TEXT_1, True, TEXT_COLOR).convert_alpha()
+            text_rect = text_one.get_rect(center=TEXT_CENTER_POSITION)
+            text_two = TEXT_FONT.render(TEXT_2, True, TEXT_COLOR).convert_alpha()
+            text_rect = text_one.get_rect(center=TEXT_CENTER_POSITION)
+            self.screen.blit(source=text_one, dest=text_rect)
+
             pygame.display.flip()
 
 
@@ -22,3 +39,9 @@ class Menu():
                     pygame.quit()
                     quit()
 
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.Font('assets\fonts\Pixelmax-Regular.otf', size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.screen.blit(source=text_surf, dest=text_rect)
