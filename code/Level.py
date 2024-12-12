001a47017852603ac2af4fc5b7f87d0453c05bdc
Lucas Messias <luquinhasmessias7@gmail.com>
2024-12-12 12:39:48 -0300

    corrigindo bug de erro ao fechar game

--- code/Level.py
+++ code/Level.py
@@ -1,6 +1,7 @@
 from code.Entity import Entity
 from code.EntityFactory import EntityFactory
 import pygame
+import sys
 
 class Level:
     
@@ -15,8 +16,17 @@ class Level:
         pygame.mixer_music.load('assets/images/Ensiferum_In_My_SwordITrust.mp3')
         pygame.mixer_music.set_volume(0.3)
         pygame.mixer_music.play(-1)
+        # clock = pygame.time.Clock()
         while True:
+            # clock.tick(60)
             for ent in self.entity_list:
                 self.windows.blit(source=ent.surf, dest=ent.rect)
                 ent.move()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
             pygame.display.flip()
+    pass
+
